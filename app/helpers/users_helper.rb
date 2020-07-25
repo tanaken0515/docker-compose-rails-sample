470b4dc83d60bc8fa4937f58513b26c2f728cc50
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2020-07-26 07:53:29 +0900

    Replace users index view to Vue.js

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+  def users_index_json(users)
+    users.map do |user|
+      {
+        name: user.name,
+        email: user.email,
+        show: user_path(user),
+        edit: edit_user_path(user),
+        destroy: user_path(user)
+      }
+    end.to_json
+  end
 end
---
+++ app/javascript/components/users/UsersIndex.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <p id="notice">{{ notice }}</p>
+
+    <h1>Users</h1>
+
+    <table>
+      <thead>
+      <tr>
+        <th>Name</th>
+        <th>Email</th>
+        <th colspan="3"></th>
+      </tr>
+      </thead>
+
+      <tbody>
+      <tr v-for="user in users">
+        <td>{{ user.name }}</td>
+        <td>{{ user.email }}</td>
+        <td><a :href="user.show">Show</a></td>
+        <td><a :href="user.edit">Edit</a></td>
+        <td><a :href="user.destroy" data-confirm="Are you sure?" rel="nofollow" data-method="delete">Destroy</a></td>
+      </tr>
+      </tbody>
+    </table>
+
+    <br>
+
+    <a :href="newUserPath">New User</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UsersIndex',
+  props: {
+    notice: {
+      type: String,
+      required: true
+    },
+    users: {
+      type: Array,
+      required: true
+    },
+    newUserPath: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
